3f987b77d68cb85aaece4f776b6d78d76779c224
Emre Dogan <415960@student.fontys.nl>
2019-04-03 00:44:00 +0200

    Algoritme testfase

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -31,41 +31,66 @@ namespace WindowsFormsApp1
         {
             if (b1.Text == b2.Text && b2.Text == b3.Text && b3.Text != "")
             {
-                win();
+                if (b1.Text == "X")
+                { win(); }
+                else
+                { win(); }
+
             }
 
             if (b4.Text == b5.Text && b5.Text == b6.Text && b6.Text != "")
             {
-                win();
+                if (b4.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
 
             if (b7.Text == b8.Text && b8.Text == b9.Text && b9.Text != "")
             {
-                win();
+                if (b7.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
 
             if (b1.Text == b4.Text && b4.Text == b7.Text && b7.Text != "")
             {
-                win();
+                if (b1.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
 
             if (b2.Text == b5.Text && b5.Text == b8.Text && b8.Text != "")
             {
-                win();
+                if (b2.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
 
             if (b3.Text == b6.Text && b6.Text == b9.Text && b9.Text != "")
             {
-                win();
+                if (b3.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
 
             if (b3.Text == b5.Text && b5.Text == b7.Text && b7.Text != "")
             {
-                win();
+                if (b3.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
             if (b1.Text == b5.Text && b5.Text == b9.Text && b9.Text != "")
             {
-                win();
+                if (b1.Text == "X")
+                { win(); }
+                else
+                { win(); }
             }
         }
         private void checkbeurt()
@@ -82,30 +107,189 @@ namespace WindowsFormsApp1
         string zet(int stap)
         {
             string welke;
-            if (stap == 0)
-            {
-                welke = "x";
-                zetnummer++;
-            }
+             if (stap == 0)
+             {
+            welke = "X";
+            zetnummer++;
+             }
             else
             {
-                welke = "o";
+               welke = "O";
                 zetnummer--;
             }
             return welke;
         }
         private void algoritme() // maak een algoritme noob //
         {
-            
+            //winning statements AI//
+            if (b1.Text == "O" && b1.Text == b2.Text && b3.Text != "" && zetnummer != 0)
+            {
+                b3.Text = "O";
+            }
+
+            if (b4.Text == "O" && b4.Text == b5.Text && b6.Text != "" && zetnummer != 0)
+            {
+                b6.Text = "O";
+            }
+
+            if (b7.Text == "O" && b7.Text == b8.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            if (b1.Text == "O" && b1.Text == b4.Text && b7.Text != "" && zetnummer != 0)
+            {
+                b7.Text = "O";
+            }
+
+            if (b2.Text == "O" && b2.Text == b5.Text && b8.Text != "" && zetnummer != 0)
+            {
+                b8.Text = "O";
+            }
+
+            if (b3.Text == "O" && b3.Text == b6.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            if (b3.Text == "O" && b3.Text == b5.Text && b7.Text != "" && zetnummer != 0)
+            {
+                b7.Text = "O";
+            }
+
+            if (b1.Text == "O" && b1.Text == b5.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            //Block enemy//
+            if (b1.Text == "X" && b1.Text == b2.Text && b3.Text != "" && zetnummer != 0)
+            {
+                b3.Text = "O";
+            }
+
+            if (b4.Text == "X" && b4.Text == b5.Text && b6.Text != "" && zetnummer != 0)
+            {
+                b6.Text = "O";
+            }
+
+            if (b7.Text == "X" && b7.Text == b8.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            if (b1.Text == "X" && b1.Text == b4.Text && b7.Text != "" && zetnummer != 0)
+            {
+                b7.Text = "O";
+            }
+
+            if (b2.Text == "X" && b2.Text == b5.Text && b8.Text != "" && zetnummer != 0)
+            {
+                b8.Text = "O";
+            }
+
+            if (b3.Text == "X" && b3.Text == b6.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            if (b3.Text == "X" && b3.Text == b5.Text && b7.Text != "" && zetnummer != 0)
+            {
+                b7.Text = "O";
+            }
+
+            if (b1.Text == "X" && b1.Text == b5.Text && b9.Text != "" && zetnummer != 0)
+            {
+                b9.Text = "O";
+            }
+
+            //AI Placement//
+            if (b1.Text == "" || b1.Text == "O" && b2.Text == "" || b2.Text == "O" && b3.Text == "" && zetnummer != 0)
+            {
+                if (b1.Text != "O" && b1.Text == "" && zetnummer != 0)
+                {
+                    b1.Text = "O";
+                    zetnummer--;
+                }
+                if (b1.Text == "O" && b2.Text == "" && zetnummer != 0)
+                {
+                    b2.Text = "O";
+                    zetnummer--;
+                }
+                if (b2.Text == "O" && b3.Text == "" && zetnummer != 0)
+                {
+                    b3.Text = "O";
+                    zetnummer--;
+                }
+            }
+
+            if (b1.Text == "" || b1.Text == "O" && b4.Text == "" || b4.Text == "O" && b7.Text == "" && zetnummer != 0)
+            {
+                if (b1.Text != "O" && b1.Text == "" && zetnummer != 0)
+                {
+                    b1.Text = "O";
+                    zetnummer--; 
+                }
+                if (b1.Text == "O" && b4.Text == "" && zetnummer != 0)
+                {
+                    b4.Text = "O";
+                    zetnummer--;
+                }
+                if (b4.Text == "O" && b7.Text == "" && zetnummer != 0)
+                {
+                    b7.Text = "O";
+                    zetnummer--;
+                }
+            }
+
+            if (b3.Text == "" || b3.Text == "O" && b6.Text == "" || b6.Text == "O" && b9.Text == "" && zetnummer != 0)
+            {
+                if (b3.Text != "O" && b3.Text == "" && zetnummer != 0)
+                {
+                    b3.Text = "O";
+                    zetnummer++;  
+                }
+                if (b3.Text == "O" && b6.Text == "" && zetnummer != 0)
+                {
+                    b6.Text = "O";
+                    zetnummer++;
+                }
+                if (b6.Text == "O" && b9.Text == "" && zetnummer != 0)
+                {
+                    b9.Text = "O";
+                    zetnummer++;
+                }
+            }
+
+            if (b7.Text == "" || b7.Text == "O" && b8.Text == "" || b8.Text == "O" && b9.Text == "" && zetnummer != 0)
+            {
+                if (b7.Text != "O" && b7.Text == ""&& zetnummer != 0)
+                {
+                    b7.Text = "O";
+                    zetnummer++; 
+                }
+                if (b7.Text == "O" && b8.Text == "" && zetnummer != 0)
+                {
+                    b8.Text = "O";
+                    zetnummer++;
+                }
+                if (b8.Text == "O" && b9.Text == "" && zetnummer != 0)
+                {
+                    b9.Text = "O";
+                    zetnummer++;
+                }
+            }
         }
         private void b1_Click(object sender, EventArgs e)
         {
             if (b1.Text == "")
             {
                 b1.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
+            
         }
 
         private void b2_Click(object sender, EventArgs e)
@@ -113,9 +297,11 @@ namespace WindowsFormsApp1
             if (b2.Text == "")
             {
                 b2.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
+            
         }
 
         private void b3_Click(object sender, EventArgs e)
@@ -123,9 +309,11 @@ namespace WindowsFormsApp1
             if (b3.Text == "")
             {
                 b3.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
+            
         }
 
         private void b4_Click(object sender, EventArgs e)
@@ -133,9 +321,11 @@ namespace WindowsFormsApp1
             if (b4.Text == "")
             {
                 b4.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
+            
         }
 
         private void b5_Click(object sender, EventArgs e)
@@ -143,9 +333,11 @@ namespace WindowsFormsApp1
             if (b5.Text == "")
             {
                 b5.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
+           
         }
 
         private void b6_Click(object sender, EventArgs e)
@@ -153,6 +345,7 @@ namespace WindowsFormsApp1
             if (b6.Text == "")
             {
                 b6.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
@@ -163,6 +356,7 @@ namespace WindowsFormsApp1
             if (b7.Text == "")
             {
                 b7.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
@@ -173,6 +367,7 @@ namespace WindowsFormsApp1
             if (b8.Text == "")
             {
                 b8.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
@@ -183,6 +378,7 @@ namespace WindowsFormsApp1
             if (b9.Text == "")
             {
                 b9.Text = zet(zetnummer);
+                algoritme();
                 checkwin();
             }
             checkbeurt();
